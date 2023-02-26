f3f08bca937413666dab385ec53ad6984d6f879e
Youenn <pierre_justin.youenn@courrier.uqam.ca>
2023-02-25 20:12:18 -0500

    Ajout fichier gitignore et pom.xml

---
+++ src/main/java/fallenkingdom/fallenkingdom/FallenKingdom.java
@@ -0,0 +1,17 @@
+package fallenkingdom.fallenkingdom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FallenKingdom extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
